cb88ed720d86f02842e044dd8d9e69495ae97703
Rémy Roy <remyroy@remyroy.com>
2022-10-04 09:46:44 -0400

    Fix issue with TestnetNotification component

--- src/components/TestnetNotification.tsx
+++ src/components/TestnetNotification.tsx
@@ -3,25 +3,19 @@ import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
 import { Alert } from './Alert';
 
+import { NETWORK_NAME } from '../utils/envVars';
+
 const StyledAlert = styled(Alert)`
   text-align: center;
-`;
-
-const AllCaps = styled.span`
-  text-transform: uppercase;
 `;
 
 export const TestnetNotification = (): JSX.Element => {
   return (
     <StyledAlert variant="error" round="none" pad="small">
       <FormattedMessage
-        defaultMessage="Warning: This is a staking launchpad for the {testnet}."
+        defaultMessage="Warning: This is a staking launchpad for the {network} testnet."
         values={{
-          testnet: (
-            <AllCaps>
-              <FormattedMessage defaultMessage="testnet" />
-            </AllCaps>
-          ),
+          network: NETWORK_NAME,
         }}
       />
     </StyledAlert>
