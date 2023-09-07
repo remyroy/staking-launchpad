210e400ef6d2fe1ea27d8f86e9cbcba98a70a048
Rémy Roy <remyroy@remyroy.com>
2023-09-07 16:26:13 -0400

    Updates for new domain names

--- src/utils/envVars.ts
+++ src/utils/envVars.ts
@@ -13,8 +13,8 @@ export const RPC_URL                    = process.env.REACT_APP_RPC_URL || INFUR
 // public
 export const NETWORK_NAME               = IS_MAINNET ? 'Mainnet' : TESTNET_LAUNCHPAD_NAME;
 export const TICKER_NAME                = IS_MAINNET ? 'ETH' : `${EL_TESTNET_NAME} ETH`;
-export const ETHERSCAN_URL              = 'https://explorer.ephemery.pk910.de/tx';
-export const BEACONCHAIN_URL            = 'https://beaconchain.ephemery.pk910.de';
+export const ETHERSCAN_URL              = 'https://explorer.ephemery.dev/tx';
+export const BEACONCHAIN_URL            = 'https://beaconchain.ephemery.dev';
 export const FORTMATIC_KEY              = process.env.REACT_APP_FORTMATIC_KEY       || 'pk_test_D113D979E0D3508F';
 export const CONTRACT_ADDRESS           = IS_MAINNET ? '0x00000000219ab540356cBB839Cbe05303d7705Fa' : process.env.REACT_APP_CONTRACT_ADDRESS;
 export const MIN_DEPOSIT_CLI_VERSION    = process.env.REACT_APP_MIN_DEPOSIT_CLI_VERSION  || '1.0.0';
